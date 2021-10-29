5305d1aae2155f8ae5500bf22ac137255e6d4593
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2021-10-29 09:24:18 +0700

    1. Handle String using TCP

--- TCPHandleString/src/main/java/com/dut/tcphandlestring/StringServer.java
+++ TCPHandleString/src/main/java/com/dut/tcphandlestring/StringServer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.tcpxulychuoi;
+package com.dut.tcphandlestring;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
